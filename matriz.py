047ba4abeb004ebd3b77a1fcebd7095228cef3e3
Enzo Esmeraldo <75813033+ensinho@users.noreply.github.com>
2022-06-29 16:41:45 -0300

    Delete matriz.py

--- matriz.py
+++
@@ -1,6 +0,0 @@
-# preenchimento de matriz
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for i in range(0, 3):
-    for j in range(0, 3):
-        matriz[i][j] = int(input(f"Digite o valor da posição [{i}, {j}]: "))
-print(matriz)
